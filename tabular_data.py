fe0eb708bfe808041efa1b017c0dfed46db3966d
wey1998 <yitwey1998@hotmail.com>
2023-01-20 21:51:06 +0000

    Add function to remove rows with missing ratings

--- tabular_data.py
+++ tabular_data.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import os
 
+def remove_rows_with_missing_ratings(dataframe,column_name):
+    
+    dataframe.dropna(subset = column_name,inplace = True)
+    
+    return dataframe
+    
+
 if __name__ == "__main__":
 
     working_dir = os.path.dirname(__file__)
     data_path = "data/tabular_data/listing.csv"
     path = os.path.join(working_dir, data_path)
     listing_df = pd.read_csv(path)
+    column_names = listing_df.columns
+    print(column_names)
+    rating_columns = column_names[10:16]
+    listing_df = remove_rows_with_missing_ratings(listing_df,rating_columns)
+
