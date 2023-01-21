bc03ed3dda380a776e2223b8c3a1ea668881955e
wey1998 <yitwey1998@hotmail.com>
2023-01-21 23:31:56 +0000

    Add set_default_feature_values function

--- tabular_data.py
+++ tabular_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 
 def remove_rows_with_missing_ratings(dataframe,column_name):
@@ -13,23 +14,46 @@ def remove_rows_with_missing_ratings(dataframe,column_name):
 
     Returns:
         pandas.DataFrame: The dataframe with the rows that contains missing values in 
-        
+        specified columns removed. 
     """
     
     dataframe.dropna(subset = column_name,inplace = True)
     
     return dataframe
+
+def combine_description_strings(dataframe):
+    pass
+
+def set_default_feature_values(dataframe):
+    col_names = ["guests","beds","bathrooms","bedrooms"]
     
+    for column in col_names:
+        
+        if dataframe[column].dtype == object:
+            dataframe[column] = dataframe[column].replace(r"[a-zA-Z]+",1,regex = True)
+        
+        
+        dataframe[column] = dataframe[column].replace(np.nan,1)
+        
+        dataframe[column] = dataframe[column].astype(float)
+        
+        print(sorted(list(dataframe[column].unique())))
+            
+        
+    return dataframe
+
 
 if __name__ == "__main__":
 
     working_dir = os.path.dirname(__file__)
     data_path = "data/tabular_data/listing.csv"
     path = os.path.join(working_dir, data_path)
-    listing_df = pd.read_csv(path)
+    listing_df = pd.read_csv(path, na_values=np.nan)
     column_names = listing_df.columns
     print(column_names)
     rating_columns = column_names[10:16]
     listing_df = remove_rows_with_missing_ratings(listing_df,rating_columns)
-    print(type(listing_df))
+    listing_df = set_default_feature_values(listing_df)
+
+
 
